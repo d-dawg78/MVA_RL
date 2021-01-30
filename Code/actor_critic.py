45b098e673903660d56d8cbebf06cac29a89ecd0
Dorian Desblancs <dorian.desblancs@mail.mcgill.ca>
2021-01-30 04:51:43 +0100

    Implemented basic actor-critic and random platform and obstacle generation.. TODO - coordinate system and wave processing.

---
+++ Code/actor_critic.py
@@ -0,0 +1,102 @@
+import numpy as np 
+import place_cell
+
+
+class Critic(object):
+    """
+    Class that defines the critic and its functions.
+    """
+
+    def __init__(self, lr, g, num_place_cells):
+        """
+        Arguments:
+        -- num_place_cells: number of place cells.
+        -- lr: learning rate
+        -- gamma: discounting factor
+        """
+
+        self.W     = np.zeros(num_place_cells)
+        self.lr    = lr
+        self.gamma = gamma
+
+
+    def weight_update(self, R, place_cells):
+        """
+        Weight update function. 
+        -- R: reward
+        -- place_cells: place cell array
+        """
+
+        prevs = np.zeros(len(place_cells))
+        currs = np.zeros(len(place_cells))
+
+        for x in range(len(place_cells)):
+            prevs[x] = place_cells[x].prevs
+            currs[x] = place_cells[x].current
+
+        C_currs = np.dot(currs, self.W)
+        C_prevs = np.dot(prevs, self.W)
+
+        error   = R + self.gamma * C_currs - C_prevs
+
+        self.W = self.W + self.lr * (error * prevs)
+
+        return error
+
+
+class Actor(object):
+    """
+    Class that defines the actor and its functions.
+    """
+
+    def __init__(self, lr, num_directions, num_place_cells):
+        """
+        Arguments:
+        -- num_directions: number of directions actor can use.
+        -- num_place_cells: total number of place cells.
+        """
+
+        self.W  = np.zeros([num_directions, num_place_cells])
+        self.lr = lr
+
+    def probs(self, place_cells):
+        """
+        Compute probabilities.
+        -- place_cells: array of place cells.
+        """
+
+        currs = np.zeros(len(place_cells))
+
+        for x in range(len(place_cells)):
+            currs[x] = place_cells[x].current
+
+        a = np.dot(self.W, currs)
+
+        # Overflow avoidance
+        max_a = np.max(a)
+        num = 2 * (a - max_a)
+        num = 2 * a
+        num = np.exp(num)
+
+        den   = num.sum()
+
+        p = num / den
+
+        return p
+
+    def weight_update(self, dir, error, place_cells):
+        """
+        Weight update function. 
+        -- dir: direction chosen at current step
+        -- error: error computed at each step
+        -- place_cells: array of place cells at each step
+        """
+
+        prevs = np.zeros(len(place_cells))
+
+        for x in range(len(place_cells)):
+            prevs[x] = place_cells[x].prevs
+
+        self.W[dir, :] = self.W[dir: :] = self.lr * error * prevs[:]
+
+        return
--- Code/main.py
+++ Code/main.py
@@ -4,29 +4,217 @@ import random
 import place_cell
 import time
 import matplotlib.pyplot as plt
+import random
+import math
+import actor_critic
 
 plt.rcParams["figure.figsize"] = (7,7)
 
-# Create world
+
+def gen_ran_coords(radius, verbose=True):
+    """
+    Function for generating random coordinates in world.
+    -- radius: world radius.
+    """
+
+    a = 2 * random.random() * math.pi
+    r = radius * math.sqrt(random.random())
+
+    x = r * math.cos(a)
+    y = r * math.sin(a)
+
+    if (verbose == True):
+        print("Platform location: {}, {}".format(x, y))
+
+    return np.array([x, y])
+
+
+def gen_obs_coords(radius, pf_coords, pf_radius, obs_diams=np.array([]), verbose=False):
+    """
+    Function for generating obstacle locations randomly.
+    -- radius: world radius
+    -- pf_coords: platform coordinates
+    -- pf_radius: platform radius
+    -- obs_diams: obstacle diamaters 
+    -- verbose: print statements or not
+    """
+
+    if (len(obs_diams) == 0):
+        obstacle_locations = np.array([])
+
+        return obstacle_locations
+
+    else:
+        obstacle_locations = np.full((len(obs_diams), 2), fill_value=[-math.inf, -math.inf])
+
+        for k in range(len(obs_diams)):
+            check = False
+
+            while (check == False):
+
+                check = True
+
+                diam = obs_diams[k]
+                x, y = gen_ran_coords(radius - 3/2 * diam, verbose=False)
+
+                if ((pf_coords[0] - pf_radius - diam <= x <= pf_coords[0] + pf_radius) and
+                    (pf_coords[1] - pf_radius - diam <= y <= pf_coords[1] + pf_radius)):
+
+                    check = False
+                    continue
+
+                if (k == 0):
+                    continue
+
+                for l in range(k):
+                    x_temp, y_temp = obstacle_locations[l]
+                    diam_temp = obs_diams[l]
+
+                    if ((x + diam < x_temp or x > x_temp + diam_temp) and 
+                        (y + diam < y_temp or y > y_temp + diam_temp)):
+
+                        continue
+                    
+                    else:
+                        check = False
+
+            obstacle_locations[k] = [x, y]
+
+        return obstacle_locations
+
+
+# Initialize everything
 world_radius = 60
+lr           = 0.5
+gamma        = 0.99
+num_trial    = 100
+trial_len    = 100
+num_days     = 36
+num_cells    = 493
+cell_std     = 3
+pf_radius    = 10
+path_len     = 100
+num_dirs     = 8
 
-world = environment.world(world_radius=world_radius, num_sound_waves=1, obstacle_locations=np.array([[20, -40], [-10, -15], [-35, -45], [-40, 5]]), obstacle_diameters=np.array([20, 30, 16, 24]))
-pc_arr = place_cell.place_cells(493, 3, world_radius, verbose=True)
+obstacle_diameters=np.array([23, 15, 10, 8])
 
-# Set starting position
-world.startposition()
+avg_path_len = np.zeros(num_days)
+avg_path_std = np.zeros(num_days)
 
-# Run one trial
-while (not world.timeup() and not world.atgoal()):
+pf_change   = False
+change_days = 4
 
-    # Select random actions
-    A = np.random.randint(0, 8)
+# Algorithm options:
+# -- td    : regular td-learning
+# -- coord : td-learning with coordinate system
+# -- both  : run both algorithms to compare performance
+algorithm   = "ahahhaha"
 
-    # Move agent
-    world.move(A)
+# Set starting position randomly
+pf_good = False
+pf_coords = gen_ran_coords(world_radius - pf_radius)
 
-    # Show waves
-    world.generate_waves(length=60, verbose=False)
+obstacle_locations = gen_obs_coords(world_radius, pf_coords, pf_radius, obstacle_diameters)
 
-# Plot path
-world.plotpath()
+pc_arr = place_cell.place_cells(num_cells, cell_std, world_radius, verbose=False)
+
+# Start trials
+if (algorithm == "td"):
+    actor  = actor_critic.Actor(lr, num_dirs, num_cells)
+    critic = actor_critic.Critic(lr, gamma, num_cells)
+
+    # Set start position
+    x_coord, y_coord = gen_ran_coords(world_radius, verbose=True)
+
+    for day in range(num_days):
+        path_len = []
+
+        # Re-set start position
+        if (pf_change == True and day % change_days == 0):
+            x_coord, y_coord = gen_ran_coords(world_radius, verbose=True)
+
+        for trial in num_trial:
+
+            # Create world and its start position
+            world  = environment.world(
+                           T=path_len,
+                           world_radius=world_radius, 
+                           num_sound_waves=0, 
+                           platform_location=pf_coords,
+                           platform_radius=pf_radius,
+                           obstacle_locations=obstacle_locations, 
+                           obstacle_diameters=obstacle_diameters)
+            
+            world.startposition()
+
+            # Run trial
+            while (not world.timeup() and not world.atgoal()):
+                
+                # Determine new position from 
+                position  = np.array(world.position[:, world.t])
+                probs     = actor.probs(pc_arr)
+                possibs   = np.linspace(0, num_dirs - 1, num=num_dirs, dtype=int)
+                direction = np.random.choice(possibs, p=probs)
+                world.move(direction)
+                new_pos   = np.array(world.position[:, world.t])
+
+                # Activate place cells
+                for x in range(len(pc_arr)):
+                    pc_arr[x].activate(new_pos)
+
+                # Determine reward
+                if (world.atgoal()):
+                    rt = 1
+                
+                else:
+                    rt = 0
+
+                # Update weights
+                error = critic.weight_update(rt, pc_arr)
+                actor.weight_update(direction, error, pc_arr)
+            
+            path_len.append(world.t)
+        
+        arr_path_len  = np.asarray(path_len)
+        mean_path_len = np.mean(arr_path_len)
+        std_path_len  = np.std(arr_path_len)
+
+        avg_path_len[day] = mean_path_len
+        avg_path_std[day] = std_path_len
+
+        print("Day {} -- Mean Escape Latency: {} +/- {}".format(day, mean_path_len, std_path_len))
+
+    plt.errorbar(np.linspace(1, num_days, num=num_days), avg_path_len, avg_path_std, linestyle='dotted', marker='o', capsize=5)
+    plt.xlabel("Day")
+    plt.ylabel("Escape Latency (s)")
+    plt.title("Pure TD Learning Escape Latency")
+    plt.show()
+
+else:
+
+    # Create world and its start position
+    world  = environment.world(
+                T=path_len,
+                world_radius=world_radius, 
+                num_sound_waves=0, 
+                platform_location=pf_coords,
+                platform_radius=pf_radius,
+                obstacle_locations=obstacle_locations, 
+                obstacle_diameters=obstacle_diameters)
+
+    world.startposition()
+
+    # Run one trial
+    while (not world.timeup() and not world.atgoal()):
+
+        # Select random actions
+        A = np.random.randint(0, num_dirs)
+
+        # Move agent
+        world.move(A)
+
+        # Show waves
+        world.generate_waves(length=60, verbose=False)
+
+    # Plot path
+    world.plotpath()
